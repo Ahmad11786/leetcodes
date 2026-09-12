46d498abaf730e42e37d849935b441cb34150dc9
Muhammad ahmad <muhammad.ahmad004531@gmail.com>
2026-09-12 13:36:20 +0500

    Time: 6 ms (75.54%) | Memory: 44.7 MB (78.52%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int count=0;
+
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+
+        int j=0;
+
+        for(int i=0;i<g.size() && j<s.size();i++)
+        {
+            if(g[i]<=s[j])
+            {
+                count++;
+                j++;
+            }
+            else
+            {
+                j++;
+                i--;
+            }
+        }
+
+        return count;
+    }
+};
