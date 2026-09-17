3631d227572f97fc3fd14ac6601e781323d28efa
Muhammad ahmad <muhammad.ahmad004531@gmail.com>
2026-09-17 14:40:41 +0500

    Time: 0 ms (100.00%) | Memory: 13 MB (80.31%) - LeetSync

---
+++ 2219-maximum-number-of-words-found-in-sentences/maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int  counts=1;
+        for(int i=0;i<sentences.size();i++)
+        { int count=1;
+            for(int j=0;j<sentences[i].size();j++)
+            {
+                if(sentences[i][j]==' '){
+                    count++;
+                }
+            }
+            if(count>counts)
+            {
+                counts=count;
+            }
+        }
+        return counts;
+    }
+};
